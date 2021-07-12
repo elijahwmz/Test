c1999cda3fe3c4daa2e3c39c196e40ea888465fc
Elijah <elijah_7674@163.com>
2021-07-12 14:03:56 +0800

    test

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
         System.out.println("Hello World!");
+        System.out.println("hello");
+        System.out.println("test");
     }
 }
